52b0ace41c3814a9cb8f7b2d8ea3340feef5a34a
Carissa Gross <carissamgross@gmail.com>
2022-11-09 09:35:11 -0700

    Update component file structure, create App Component

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      hikes: [],
+      error: ''
+    }
+  }
+
+  render() {
+    return (
+      <main className='App'>
+      </main>
+    )
+  }
+}
+export default App;
